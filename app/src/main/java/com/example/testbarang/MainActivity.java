94faea6b5718407fc17739f380c79390bfae9ace
Adelina Yoga Eka Safira <aaderina57@gmail.com>
2020-06-08 14:06:36 +0700

    Add fungsi lihat barang

--- app/src/main/java/com/example/testbarang/MainActivity.java
+++ app/src/main/java/com/example/testbarang/MainActivity.java
@@ -26,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
         });
         bLihat.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-
+            public void onClick(View v) {
+                startActivity(LihatBarang.getActIntent(MainActivity.this));
             }
         });
     }
